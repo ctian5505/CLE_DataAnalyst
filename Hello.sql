fcf572119de8a92ad7c1b66e2c86390035e18865
ctian5505 <66417615+ctian5505@users.noreply.github.com>
2025-02-05 15:34:26 +0800

    Update Hello.sql

--- Hello.sql
+++ Hello.sql
@@ -1,1 +1,15 @@
+/*
+AdventureWorks sample databases Query Samples
+Files : AdventureWorksDW2022.bak (https://learn.microsoft.com/en-us/sql/samples/adventureworks-install-configure?view=sql-server-ver16&tabs=ssms
+)
+*/
 
+-- Find the top 10 customers together with their customer key, first name, last name, and email address.
+SELECT 
+	TOP 10
+	CustomerKey,
+	FirstName,
+	LastName,
+	EmailAddress	
+FROM 
+	DimCustomer
